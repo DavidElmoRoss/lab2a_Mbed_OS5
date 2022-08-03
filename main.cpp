0367ffc8c9436cc7907ff878dfc2714a4d04e953
David Elmo Ross <david.elmo.ross@icloud.com>
2022-08-03 17:41:27 +0000

    updated to same code as OS2 version
    Signed-off-by: David Elmo Ross <david.elmo.ross@icloud.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title:  Program to generate output to the PC screen
+    Title:  Program to generate output to the PC screen using MBED OS5 Library
     Author: Your Name, Student ID, Course CODE, Lab Section
     Date:   Today's Date
     Description:    This program will take characters from the keyboard and
@@ -12,12 +12,17 @@
  
 int main(void)
 {
-    unsigned char input;    // variable to get char from keyboard
+    unsigned char input;        // variable to get char from keyboard
 
-    for(;;) {
+    for(;;)                     // endless loop
+    {
         input=getc(stdin);      // get char and put into variable input
-        //  if(input == 0x0d)     // if input is <RETURN>
-        //  putc(0x0a,stdout);    // output a <LINE FEED>
+        putc(input,stdout);     // send char to output buffer
+          if(input == 0x0d)     // if input is <RETURN>
+          {
+             putc(0x0a,stdout); // send <LF> to output buffer
+          }
+          fflush(stdout);       // flush output buffer to screen
    
     }
 }
